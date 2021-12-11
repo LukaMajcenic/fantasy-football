cbbe54a7394cc018305432ab124732151cf75b2d
Majcenic <80134096+Majcenic@users.noreply.github.com>
2021-12-11 18:40:07 +0100

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FantasyFootball());
+}
+
+class FantasyFootball extends StatelessWidget {
+  const FantasyFootball({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+      ),
+    );
+  }
+}
