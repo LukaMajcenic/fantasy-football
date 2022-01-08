fbb4a1f08db921791c46b30812e1a0a1999db318
Majcenic <80134096+Majcenic@users.noreply.github.com>
2022-01-08 13:41:25 +0100

    Nothing works...

---
+++ lib/pages/main2.dart
@@ -0,0 +1,17 @@
+/* import 'package:flutter/material.dart';
+
+class Main2 extends StatelessWidget {
+  const Main2({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: ,
+        builder: (context, data) {
+
+        },
+      ),
+    );
+  }
+} */
